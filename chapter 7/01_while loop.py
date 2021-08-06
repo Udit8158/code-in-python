7ad8ca6a461edd4938d7ca3a4ec5c42a942eb5d8
Udit8158 <kunduudit8158@gmail.com>
2021-08-06 13:44:52 +0530

    add new files

---
+++ chapter 7/01_while loop.py
@@ -0,0 +1,6 @@
+# Date:05/08/2021
+a=0
+# a=a+1          #we cant do this here
+while a<=10:     #while loop is running untill the condition is going to true.
+    print(a)
+    a=a+1          #we can do it here in the while loop
---
+++ chapter 7/02_quiz.py
@@ -0,0 +1,13 @@
+# # Date:05/08/2021
+# printing 1 to 50 by usig while loop
+num=1
+while num<=50:
+    print(num)
+    num=num+1  
+
+
+list=[45,67,34,89]
+a=0
+while a<len(list):
+    print(list[a])
+    a=a+1
