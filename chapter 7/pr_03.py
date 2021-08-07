5e3f3e1040a0385f6fe1e6b4442206537dbce99c
Udit8158 <kunduudit8158@gmail.com>
2021-08-07 18:23:04 +0530

    add pr_03.py

---
+++ chapter 7/pr_03.py
@@ -0,0 +1,9 @@
+# Date:07/08/2021
+# Doing multiply of a givven number by while loop
+num=int(input("Enter a number:\n"))
+a=num
+i=1
+while i<=10:
+    m=a*i
+    print(str(num)+"X"+str(i)+"="+str(m))
+    i=i+1
