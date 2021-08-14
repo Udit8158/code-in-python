7599492d362bb71cca866c7271ab19c07a67477c
Udit8158 <kunduudit8158@gmail.com>
2021-08-14 13:40:48 +0530

    add

---
+++ chapter 10/02_oops_railway_application.py
@@ -0,0 +1,15 @@
+# Date:14/08/2021
+# Creating a RailwayApplication
+
+class RailwayApplication():     #definig the class
+    def PrintData(self):        #definig a method by function
+        print(f"Name is {self.Name}")
+        print(f"Train is {self.Train}")
+        print(f"Adress is {self.Adress}")
+        print(f"Date is {self.Date}")
+uditsapplication=RailwayApplication()   #introducing the class in the code     
+uditsapplication.Name="Udit"            
+uditsapplication.Train="Rajdhani"
+uditsapplication.Adress="Westbengal"
+uditsapplication.Date="14th august"
+uditsapplication.PrintData()
---
+++ chapter 10/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+ {self.Name}")
+        print(f"Train {self.Train}")
+        # print(f"Adress: {self.Adress}")
+uditsapplication=Railway
