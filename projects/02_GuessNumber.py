d1af1c2d031d22ceba7ecd0b0f343b74b8fbb856
Udit8158 <kunduudit8158@gmail.com>
2021-08-19 20:39:48 +0530

    changeable
    Modify this file

--- projects/02_GuessNumber.py
+++ projects/02_GuessNumber.py
@@ -2,13 +2,14 @@
 #Generating a lottery type system by using python
 import random  #Importing Random
 
-lowRange = 30
-highRange = 40   #Defining Range to chose the user's number
+lowRange = 3
+highRange = 7  #Defining Range to chose the user's number
 n = random.randint(lowRange, highRange)  #Generating a random range
 turnsToFind = 5  #Defining user's turn
 e = turnsToFind-1 #Just for my comfort
 
 
+print(f"The unknown number was{n}")
 print(f"Try to guess a number which in hidden,but the number is in range between {lowRange} to {highRange}")
 print(f"You have only {turnsToFind} turns")
 
@@ -18,14 +19,17 @@ while True:
     if g > highRange or g < lowRange:
         print("Your number is not in givven range")
         break
-    if e <= 0:
+    if e < 0:
         print("Your turn is over")
         break
     if g == n:
-        print("You guess sucessfully")
-        break
+        for i in range(0,e+1):  #Trying to show the turn number where user crackit , but i can't
+            print(f"You guess sucessfully in your {i} number turn")
+            break
     else:
         print(f"Try again,you entered a wrong guess\nYou left {e} turns")
         e = e-1
         continue
-print(f"The unknown number was{n}")
+
+
+# print(f"The unknown number was{n}")
