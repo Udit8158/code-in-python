4047e2d65acf682c634747893677574c364ef258
Udit8158 <kunduudit8158@gmail.com>
2021-08-22 18:41:30 +0530

    change the new changes

--- projects/02_GuessNumber.py
+++ projects/02_GuessNumber.py
@@ -7,38 +7,54 @@ highRange = 40  #Defining Range to chose the user's number
 randomNo= random.randint(lowRange, highRange)  #Generating a random range
 turnsToFind = 10  #Defining user's turn
 
-# print(randomNo)
-print(f"Hi,\nI am a guess game system\nPlease Enter your guess between {lowRange} to {highRange}\nBut be careful you have only {turnsToFind} turns to do this")
+# print(randomNo) # It only use for emergency.
+print(f"Hi,\nI am a guess game system\nPlease Enter your guess between {lowRange} to {highRange}")
 
 userguess=None
 userturn=0
 
 while (userguess!=randomNo):       #condition for stop the loop
-    userguess=int(input("Enter your guess: "))   #Take a number from user to run the game 
+    try:
+        userguess=int(input("Enter your guess: "))   #Take a number from user to run the game 
     #Do some conditional statements
-    if userguess==randomNo:
-        userturn+=1
-        print(f"You entered a right guess in {userturn} turns")
-        break
+        if userguess==randomNo:
+            userturn+=1
+            print(f"You entered a right guess in {userturn} turns")
+            break
     
-    elif userguess<randomNo:
-        userturn+=1
-        print(f"Enter a big number\nIt is your {userturn} turn")
-        continue
+        elif userguess<randomNo:
+            userturn+=1
+            print(f"Enter a big number\nIt is your {userturn} turn")
+            continue
     
-    elif userguess>randomNo:
-        userturn+=1
-        print(f"Enter a small number\nIt is your {userturn} turn")
-        continue
+        elif userguess>randomNo:
+            userturn+=1
+            print(f"Enter a small number\nIt is your {userturn} turn")
+            continue
+    # except:
+    #      if userguess=="q":
+    #         print("Thanks for using our game")
+            # break
+    except:
+            print("Something went wrong\nPlease enter a valid number")
     
     
 #Open a file to save the high score
-with open("Highscore.txt","r") as f:
-    Highscore=int(f.read())
-#condition to save the high score
-if userturn<Highscore:
-    print(f"Oh!You just broke high score and your new score is {userturn} number of guesses. ")
-    with open("Highscore.txt","w") as f:
-        f.write(str(userturn))
+# with open('E:\\Python\\projects','r') as f:
+#     Highscore=int(f.read())
+# #condition to save the high score
+# if userturn<Highscore:
+#     print(f"Oh!You just broke high score and your new score is {userturn} number of guesses. ")
+#     with open('E:\\Python\\projects','w') as f:
+#         f.write(str(userturn))
 
     
+with open('Highscore.txt','w') as f:
+    f.write('100000')
+with open('Highscore.txt','r') as f:
+    Highscore=int(f.read())
+
+if Highscore>userturn:
+    print("Congratulation you break the highscore")
+    with open('Highscore.txt','w') as f:
+        f.write(str(userturn))
