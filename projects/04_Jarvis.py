a1d5fb7d47cc27332072ee06a968df5b6d95a274
Udit8158 <kunduudit8158@gmail.com>
2021-08-28 22:38:42 +0530

    add all

--- projects/04_Jarvis.py
+++ projects/04_Jarvis.py
@@ -1,3 +1,6 @@
+         ###  **** JARVIS  DESKTOP  VOICE  ASSISTENT ****  ###    Date :27/08/2021  starts....on that day.
+
+# Impoting some required modules
 import pyttsx3
 import datetime
 import speech_recognition as sr
@@ -6,7 +9,7 @@ import webbrowser
 import os
 import random
 
-
+# Writting some functions
 def speak(audio):
     ''' 
     used for play the audio using pyttsx3 module
@@ -17,12 +20,22 @@ def speak(audio):
 
 def wishme(user):
     '''this used to wish and introduce itself'''
-    if hour > 12:
+    if hour > 12 and hour<18:
         speak("Hi,I am Jarvis, ")
         speak(f"Good Afternoon {user} How may I help you")
-    elif hour<=12:
+
+    elif hour>=18 and hour<23:
+        speak("Hi, I am Jarvis ")
+        speak(f"Good evening {user} How may I help you")
+    
+    elif hour>=23 and hour<=6:
+        speak("Hi, I am Jarvis ")
+        speak(f"Good night {user} How may I help you")
+
+    elif hour<=12 and hour>6:
        speak("Hi,I am Jarvis, ")
        speak(f"Good Morning {user} How may I help you")
+
 def takeCommand():
     '''It takes microphone input from the user and returns string output'''
     r = sr.Recognizer()
@@ -43,10 +56,42 @@ def takeCommand():
         return "None"
     return query
 
+def playmusic():
+    '''It play just random music from my given path {music_dir} by using os module'''
+    music_dir = "D:\\music"
+    songs = os.listdir(music_dir)
+    speak("playing a random music")
+    print(songs)
+    os.startfile(os.path.join(music_dir,songs[randomNo]))
+
+def openbrowser(site_name):
+    '''By the help of webbrowser module it search the site in browser'''
+    speak(f"opening {site_name}")
+    webbrowser.open(site_name)
+
+def Introduce():
+    '''Jarvis introduce himself'''
+    speak("Hi,I am Jarvis,a desktop voice assistent made by Udit on 27th July,2021.I can do some limited number of work for you.So how can I help you sir.")
+
+def telltime():
+    '''Jarvis just speak time with the help of datetime module'''
+    time_hour = datetime.datetime.now().strftime("%H")
+    time_minute = datetime.datetime.now().strftime("%M")
+    speak(f"The time is {time_hour}:{time_minute}")
+
+def telldate():
+    '''Tell full version of date using datetime module'''
+    day = datetime.datetime.now().strftime("%d")
+    month = datetime.datetime.now().strftime("%B")
+    year = datetime.datetime.now().strftime("Y")
+    speak(day,month,year)
+
 
 if __name__=="__main__":
     
-    engine = pyttsx3.init()
+    # writting some variable which are used in code 
+
+    engine = pyttsx3.init()   #To use
     hour =int(datetime.datetime.now().hour)
     randomNo = random.randint(0,10 )
     # user = input("Enter your name :")
@@ -61,32 +106,38 @@ if __name__=="__main__":
         if 'wikipedia' in query:
             print("Searching from wikipedia")
             query = query.replace('wikipedea','')
-            result = wikipedia.summary(query,sentences=3)
+            result = wikipedia.summary(query,sentences=3)  
             print(result)
             speak(result)
         
         elif "open google" in query:   #Open any website using webbrowser module
-            speak("opening google...")
-            webbrowser.open("google.com")
+            openbrowser("google.com")
 
         elif "open youtube" in query:
-            speak("opening youtube")
-            webbrowser.open("youtube.com")
+            openbrowser("youtube.com")
+        
+        elif "open github" in query:
+            openbrowser("github.com")
+
+        elif "introduce" or "introduction" or "who are you" in query:
+            Introduce() 
+
+        elif "time"  in query:
+            telltime()  
+
+        elif "date" in query:
+            telldate()
 
         
-        elif "play music " or "play song" in query:    #Playing music with the help of os module 
-            music_dir = "D:\\music"
-            songs = os.listdir(music_dir)
-            speak("playing a random music")
-            print(songs)
-            os.startfile(os.path.join(music_dir,songs[randomNo]))
+        # elif "play music " or "play song" in query:    #Playing music with the help of os module 
+        #     playmusic()
         
         elif "close" or "stop" in query:            #for close
             speak(f"I am closing")
             break
 
+
        
     
     
    
-    
--- projects/test.py
+++ projects/test.py
@@ -1,19 +1,34 @@
 import webbrowser
-from playsound import playsound  
+from playsound import playsound
+import datetime
+from playsound import playsound
+ 
 # webbrowser.open("google.com")
     
-import os
-# import random
-# randomNo = random.randint(0, 10)
+# import os
+# # import random
+# # randomNo = random.randint(0, 10)
 
-# music_dir="D:\\Music"
-# songs= os.listdir(music_dir)
-# print(songs)
-# os.startfile(os.path.join(music_dir,songs[randomNo]))
-# playsound("/D:/Music/to/'It's realme.mp3'",True)
+# # music_dir="D:\\Music"
+# # songs= os.listdir(music_dir)
+# # print(songs)
+# # os.startfile(os.path.join(music_dir,songs[randomNo]))
+# # playsound("/D:/Music/to/'It's realme.mp3'",True)
 
-def add(a,b):
-    '''This is a function to add two numbers'''
-    return a+b
-print(add(4,5))
-print(add.__doc__)
+# def add(a,b):
+#     '''This is a function to add two numbers'''
+#     return a+b
+# print(add(4,5))
+# print(add.__doc__)
+
+# def openbrowser(site_name):
+#     # speak(f"opening {site_name}")
+#     webbrowser.open(site_name)
+
+# openbrowser("google.com")
+# d = datetime.datetime.now().strftime("%d")
+# m = datetime.datetime.now().strftime("%B")
+# y = datetime.datetime.now().strftime("%Y")
+# print(d,m,y)
+
+playsound(r'E:\\Python\\projects\\song.mp3')
